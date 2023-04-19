39c1dfc58fccf9e681a1784598e6b7a6c5fc0fa6
ITPROVN86 <quangltn3@fe.edu.vn>
2023-04-19 18:30:51 +0700

    first commit

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable Get()
+        {
+            using (EmployeeDBContext db = new EmployeeDBContext())
+            {
+                return db.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext db = new EmployeeDBContext())
+            {
+                return db.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+        // POST api/Users/CreateNew
+        [HttpPost]
+        public HttpResponseMessage CreateNew(Employee u)
+        {
+            try
+            {
+                using (EmployeeDBContext db = new EmployeeDBContext())
+                {
+                    db.Employees.Add(u);
+                    db.SaveChanges();
+                    var reponse = Request.CreateResponse(HttpStatusCode.Created, u);
+                    reponse.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = u.ID }));
+                    return reponse;
+                }
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateEmployee(Employee u)
+        {
+            using (EmployeeDBContext db = new EmployeeDBContext())
+            {
+                var exist = db.Employees.Find(u.ID);
+                if (exist != null)
+                {
+                    exist.FirstName = u.FirstName;
+                    exist.LastName = u.LastName;
+                    exist.Gender= u.Gender;
+                    exist.Salary = u.Salary;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                return Ok(exist);
+            }
+            
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            using (EmployeeDBContext db = new EmployeeDBContext())
+            {
+                var exist = db.Employees.Find(id);
+                if (exist != null)
+                {
+                    db.Employees.Remove(exist);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+                return Ok(exist);
+            }
+
+        }
+    }
+}
---
+++ EmployeeService/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index(Employee user)
+        {
+            ViewBag.Title = "Home Page";
+            var list = await GetAllUser();
+            if (list != null)       // Nếu list user khác null thì trả về View có chứa list 
+                return View(list);
+            return View();
+        }
+        private async Task<List<Employee>> GetAllUser() // Hàm Gọi API trả về list user 
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/"; // Lấy base uri của website 
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage res = await httpClient.GetAsync(baseUrl + "api/Employees");
+                if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<Employee> list = new List<Employee>();
+                    list = res.Content.ReadAsAsync<List<Employee>>().Result; return list;
+                }
+                return null;
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Employee e)
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/"; // Lấy base uri của website 
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = httpClient.PostAsJsonAsync(baseUrl + "api/Employees", e).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(e);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            EmployeeDBContext db = new EmployeeDBContext();
+            var emp = db.Employees.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Employee e)
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/"; // Lấy base uri của website 
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = httpClient.PutAsJsonAsync(baseUrl + "api/Employees", e).Result;
+                //response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(e);
+        }
+
+     
+        public async Task<ActionResult> Delete(int id)
+        {
+            string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/"; // Lấy base uri của website 
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = httpClient.DeleteAsync(baseUrl + "api/Employees/"+id).Result;
+                //response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+    }
+}
