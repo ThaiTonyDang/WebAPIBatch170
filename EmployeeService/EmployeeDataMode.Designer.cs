8c012b0b757d5f483fe239327bed0bf2714b34c5
Thai Dang <dangtranthaidtv50dh@gmail.com>
2023-04-19 21:12:04 +0700

    git init

--- EmployeeService/EmployeeDataMode.Designer.cs
+++ EmployeeService/EmployeeDataMode.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ACER\source\repos\EAP9\EmployeeService\EmployeeDataMode.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Tony\Desktop\New folder\WebAPIBatch170_172\EmployeeService\EmployeeDataMode.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
